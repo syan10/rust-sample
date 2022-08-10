2901a7968f1f0d8755680cfbd2d70511359a3f71
Yan, Shaopu <shaopu.yan@intel.com>
2022-08-10 23:28:47 +0800

    update variable sample code to add array usecase
    Signed-off-by: Yan, Shaopu <shaopu.yan@intel.com>

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -53,7 +53,7 @@ fn main() {
     println!("tup is {}, {}, {}", tup.0, tup.1, tup.2);
 
     //array
-    let mut a = [1, 2, 3, 4, 5];
+    let a = [1, 2, 3, 4, 5];
     println!("a[2] is {}", a[2]);
 
     let b: [i32; 5]= [1, 2, 3, 4, 5];
@@ -61,6 +61,17 @@ fn main() {
 
     let c = [4; 5];
     println!("c[2] is {}", c[2]);
+
+
+
+    //test_array1();
+    test_array2();
+}
+
+fn test_array1() {
+    println!("run the test of test_array1");
+
+    let a: [u8; 3] = [1, 2, 3];
 
     println!("Please input the index of array a[]");
 
@@ -77,4 +88,22 @@ fn main() {
 
     let element = a[index];
     println!("the value of element at index {} is: {}", index, element);
+}
+
+fn test_array2() {
+    println!("run the test of test_array2");
+
+    let mut arr: [u8; 1000] = [0; 1000];
+
+    for a in &arr {
+        let b = a+1;
+        print!("{} ", b);
+    }
+    println!();
+
+    for i in 0..arr.len() {
+        arr[i] = arr[i] + i as u8;
+        println!("i as u8 is {}, data is {} ", i as u8, arr[i]);
+    }
+    println!();
 }
