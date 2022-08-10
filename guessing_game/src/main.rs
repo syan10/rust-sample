1d592d3de4301c29933a92999c87b514404a4045
Yan, Shaopu <shaopu.yan@intel.com>
2022-08-10 21:01:21 +0800

    update guessing_game support random secreat
    Signed-off-by: Yan, Shaopu <shaopu.yan@intel.com>

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,13 +1,37 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
 
-    println!("Please input your guess");
+    let secret_number = rand::thread_rng().gen_range(1..101);
 
-    let mut guess = String::new();
+    println!("The secret number is: {}", secret_number);
 
-    io::stdin().read_line(&mut guess).expect("Failed to read line");
+    //loop
+    loop {
+        println!("Please input your guess");
 
-    println!("you guessed: {}", guess);
+        let mut guess = String::new();
+    
+        io::stdin().read_line(&mut guess).expect("Failed to read line");
+    
+        let guess:u32 = match guess.trim().parse() {
+            Ok(num) =>num,
+            Err(_) => continue,
+        };
+    
+        println!("you guessed: {}", guess);
+    
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+
 }
